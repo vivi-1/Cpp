7408b5eaea0d927441bf75245ead94b6a346d281
Wei Wang <wwei6@vt.edu>
2020-12-10 21:15:54 -0500

    e3.11

--- CppPrimerPractice.cpp
+++ CppPrimerPractice.cpp
@@ -380,12 +380,67 @@ if (s3=s4) {
 std::string str5= "Kevin";
 for (auto &c : str5) cout << "now our str is " << str5 << " please enter your new characters one by one" << endl && cin >> c;
 cout << "now our string is " << str5 << endl;
-*/
 
 //E3.7
 std::string str5= "Kevin";
 for (char &c : str5) cout << "now our str is " << str5 << " please enter your new character one by one" << endl && cin >> c;
 cout << "now our string is " << str5 << endl;
+
+string word;
+cin >> word;
+for (char &c : word){
+	c = 'X';
+}
+cout << word << endl;
+
+//E3.8
+//while loop
+string word, temp;
+cin >> word;
+temp = word;
+int n=0;
+while (n < word.size()) {
+	word[n] = 'X';
+	n++;
+	cout << n <<endl;
+}
+cout << "while: " << word << endl;
+
+//tranditional for loop
+word = temp;
+for (int i = 0; i < word.size(); i++) {
+	word[i] = 'X';
+	cout << i << endl;
+
+}
+cout << "tranditional for: " << word << endl;
+
+
+string s;
+cout << s[0] << "s is:" << s << ";haha"<< endl; //doesn't give error but s will not cout anything
+cout << s[1] << "s1 is:" << s << ";haha"<< endl; //
+
+
+//E3.9 It is invalid because of undefined behavior but it won't give an error if you run it . s is not initiated.
+
+//E3.10
+string phrase, newPhrase;
+int i=0;
+getline(cin, phrase);
+for (int n=0; n < phrase.size(); n++){
+	//if (!ispunct(phrase[n])) newPhrase += phrase[n]; //Method 1
+	if (ispunct(phrase[n])) {
+		phrase.erase(phrase.begin()+n);
+		n--;
+	} //Method 2
+	cout << n << endl;
+}
+	cout << phrase << ":is phrase" << endl << newPhrase << ": is newphrase" <<endl;
+	
+   //E3.11
+   The range for loop is legal (note that the statement in {} is commented. If it was not commented we need to think of cases that c value was changed in the statement.)
+   c is a reference to a constant character
+   */
 
 return 0; //return EXIT_SUCCESS
 //return(0) is basically used to tell the machine that program executed successfully.And any other number other than 0(like -1,1,2 etc..) indicate that program doesnt executed successfully. 
