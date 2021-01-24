a6843c8447c479d887ed6a90d76a21b4c242c1b2
Wei Wang <wwei6@vt.edu>
2021-01-24 00:14:22 -0500

    E

--- CppPrimerBook/CppPrimerPractice.cpp
+++ CppPrimerBook/CppPrimerPractice.cpp
@@ -1688,6 +1688,52 @@ for (string line2; line2 != "end"; ) {
 //should return true.
 
 
+//E5.18: Explain each of the following loops. Correct any problems you detect.
+
+//(a) do
+//    int v1, v2;
+//    cout << "Please enter two numbers to sum:" ;
+//    if (cin >> v1 >> v2)
+//        cout << "Sum is: " << v1 + v2 << endl;
+//while (cin);
+
+//(b) do {
+//    // . . .
+//} while (int ival = get_response());
+
+//(c) do {
+//    int ival = get_response();
+// } while (ival);
+
+
+//E5.19: Write a program that uses a do while loop to repetitively request
+//two strings from the user and report which string is less than the other.
+
+//E5.20: Write a program to read a sequence of strings from the standard
+//input until either the same word occurs twice in succession or all the words
+//have been read. Use a while loop to read the text one word at a time. Use the
+//break statement to terminate the loop if a word occurs twice in succession.
+//Print the word if it occurs twice in succession, or else print a message saying
+//that no word was repeated.
+
+//E5.21 Exercise 5.21: Revise the program from the exercise in § 5.5.1 (p. 191)
+//so that it looks only for duplicated words that start with an uppercase letter.
+
+//E5.22 The last example in this section that jumped back to begin could be better
+//written using a loop. Rewrite the code to eliminate the goto.
+
+
+//E5.23: Write a program that reads two integers from the standard input and
+//prints the result of dividing the first number by the second.
+
+//E5.24: Revise your program to throw an exception if the second number is zero.
+// Test your program with a zero input to see what happens on your system if you don’t catch an exception.
+
+//E5.25: Revise your program from the previous exercise to use a try block to
+//catch the exception. The catch clause should print a message to the user and
+//ask them to supply a new number and repeat the code inside the try.
+
+
 
 
 
