27c5ae4e57c072aa32f3d85821f94855a5214722
Wei Wang <wwei6@vt.edu>
2021-01-25 16:06:59 -0500

    E6

--- CppPrimerBook/CppPrimerPractice.cpp
+++ CppPrimerBook/CppPrimerPractice.cpp
@@ -1890,15 +1890,147 @@ int input1, input2;
   }
 */
 
-//E6.1
+//E6.1 What is the difference between a parameter and an argument?
 
 
-//E6.2
 
 
-//E6.3
+//E6.2  Indicate which of the following functions are in error and why.
+//Suggest how you might correct the problems.
+//(a) int f() {
+//     string s;
+//     // ...
+//     return s;
+// }
+
+//(b) f2(int i) { /* ... */ }
+
+//(c) int calc(int v1, int v1) /* ... */ }
+
+//(d) double square(double x) return x * x;
 
 
+
+//E6.3 Write and test your own version of fact.
+
+
+
+
+
+//E6.4: Write a function that interacts with the user, asking for a number and
+//generating the factorial of that number. Call this function from main.
+
+
+
+//E6.5: Write a function to return the absolute value of its argument.
+
+
+
+
+//E6.6 Explain the differences between a parameter, a local variable, and a
+//local static variable. Give an example of a function in which each might
+//be useful.
+
+
+
+
+//E6.7 Write a function that returns 0 when it is first called and then
+//generates numbers in sequence each time it is called again.
+
+
+
+
+//E6.8 Write a header file named Chapter6.h that contains declarations for the
+//functions you wrote for the exercises in § 6.1 (p. 205).
+
+
+//E6.9 Write your own versions of the fact.cc and factMain.cc files. These files
+//should include your Chapter6.h from the exercises in the previous section.
+//Use these files to understand how your compiler supports separate compilation.
+
+
+
+//E6.10 Using pointers, write a function to swap the values of two ints. Test
+//the function by calling it and printing the swapped values.
+
+
+
+
+//E6.11  Write and test your own version of reset that takes a reference.
+
+
+
+
+//E6.12 Rewrite the program from exercise 6.10 in § 6.2.1 (p. 210) to use
+//references instead of pointers to swap the value of two ints. Which version
+//do you think would be easier to use and why?
+
+
+
+
+
+//E6.13 Assuming T is the name of a type, explain the difference between a
+//function declared as void f(T) and void f(T&).
+
+
+
+
+
+
+
+//E6.14 Give an example of when a parameter should be a reference type.
+//Give an example of when a parameter should not be a reference.
+
+
+//E6.15 Explain the rationale for the type of each of find_char’s parameters
+//In particular, why is s a reference to const but occurs is a plain reference?
+//Why are these parameters references, but the char parameter c is not?
+//What would happen if we made s a plain reference? What if we made occurs a
+//reference to const?
+
+
+
+//E6.16 The following function, although legal, is less useful than it might be.
+//Identify and correct the limitation on this function:
+
+//bool is_empty(string& s) { return s.empty(); }
+
+
+
+//E6.17 Write a function to determine whether a string contains any capital
+//letters. Write a function to change a string to all lowercase. Do the parameters
+//you used in these functions have the same type? If so, why? If not, why not?
+
+
+
+//E6.18 Write declarations for each of the following functions. When you write
+//these declarations, use the name of the function to indicate what the function does.
+
+//(a) A function named compare that returns a bool and has two parameters that
+//are references to a class named matrix.
+
+//(b) A function named change_val that returns a vector<int> iterator and takes
+//two parameters: One is an int and the other is an iterator for a vector<int>.
+
+
+//E6.19 Given the following declarations, determine which calls are legal and
+//which are illegal. For those that are illegal, explain why.
+
+//double calc(double);
+//int count(const string &, char);
+//int sum(vector<int>::iterator, vector<int>::iterator, int);
+//vector<int> vec(10);
+//(a) calc(23.4, 55.1);
+
+//(b) count("abcda", 'a');
+
+//(c) calc(66);
+
+//(d) sum(vec.begin(), vec.end(), 3.8);
+
+
+//E6.20 When should reference parameters be references to const? What happens if
+//we make a parameter a plain reference when it could be a reference to const?
 
 
 
