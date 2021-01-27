84ead1fc00977c800df275d656724905962266cc
Wei Wang <wwei6@vt.edudu>
2021-01-27 18:43:09 -0500

    .h

--- CppPrimerBook/Chapter6.h
+++ CppPrimerBook/Chapter6.h
@@ -1,27 +1,7 @@
-#include<iostream>
-#include "Sales_item.h"
-#include<vector>
-using namespace std;
-
 
 //E6.3 6.4
-int fact(int input) {
-  int temp = 1;
-  while (input > 1) {
-    temp *= input;
-    --input;
-  }
-  return temp;
-}
-
+int fact(int);
 
 
 //E6.5
-unsigned int abv(signed int input1) {
-  if (input1 < 0) input1 *= (-1);
-  return input1;
-}
-
-
-int main()
-{
+unsigned int abv(signed int);
