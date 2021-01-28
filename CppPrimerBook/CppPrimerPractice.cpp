1d8be86cfa6df1658d3e17a8f96511a51bed5a91
Wei Wang <wwei6@vt.edudu>
2021-01-27 23:17:05 -0500

    v

--- CppPrimerBook/CppPrimerPractice.cpp
+++ CppPrimerBook/CppPrimerPractice.cpp
@@ -2133,6 +2133,7 @@ while (cin >> input5 >> input6) {
 
 
 
+
 //E6.17 Write a function to determine whether a string contains any capital
 //letters. Write a function to change a string to all lowercase. Do the parameters
 //you used in these functions have the same type? If so, why? If not, why not?
