5698e1829a325bbe74e7202f4060995ffaeab3da
Wei <wwei6@vt.edu>
2021-02-02 22:29:34 -0500

    Create a new practice file

---
+++ Effective Modern Cpp/practice.cpp
@@ -0,0 +1,25 @@
+/*
+This is CppPrimerPractice for the C++ primer 5th edition
+By Wei Wang, link: https://github.com/vivi-1/Cpp.git
+*/
+#include<iostream>
+#include "Sales_item.h"
+#include "Chapter6.h"
+#include<vector>
+using namespace std;
+using namespace ft;
+
+int main() {
+//Chapter 1
+
+//Chapter2
+//Chapter3
+//Chapter4
+//Chapter5
+//Chapter6
+//Chapter7
+//Chapter8
+
+
+  return 0;
+}
