374e52417170308259dbbbc1cd2f22f9fe6c3c0c
vivi-1 <wwei6@vt.edu>
2021-02-19 11:41:26 -0500

    header file

--- CppPrimerBook/Sales_data.h
+++ CppPrimerBook/Sales_data.h
@@ -1,15 +1,27 @@
 #ifndef SALES_DATA_H
 #define SALES_DATA_H
-#include <string>
+#include <iostream>
+using namespace std;
+
 struct Sales_data {
-    std::string bookNo;
+    string bookNo;
     unsigned units_sold = 0;
     unsigned total_units_sold = 0;
+    double price = 0.0;
     double revenue = 0.0;
     double total_revenue = 0.0;
 };
 #endif
 
-void sum_sale(Sales_data item1, Sales_data item2) {
-  
+Sales_data sum_sale_sameBookNo(Sales_data item1, Sales_data item2) {
+  Sales_data total;
+  if (item1.bookNo == item1.bookNo){
+    total.total_units_sold = item1.units_sold + item1.units_sold;
+    total.total_revenue = item1.revenue + item1.revenue;
+    return total;
+  }
+  else {
+    cout << "BookNo should be the same\n";
+    return -1;
+  }
 }
