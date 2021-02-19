d3118b1f4a08ab5ea90ece174e46e8b526c1f606
vivi-1 <wwei6@vt.edu>
2021-02-18 21:19:49 -0500

    Chapter2 end

--- CppPrimerBook/Sales_data.h
+++ CppPrimerBook/Sales_data.h
@@ -4,6 +4,12 @@
 struct Sales_data {
     std::string bookNo;
     unsigned units_sold = 0;
+    unsigned total_units_sold = 0;
     double revenue = 0.0;
+    double total_revenue = 0.0;
 };
 #endif
+
+void sum_sale(Sales_data item1, Sales_data item2) {
+  
+}
