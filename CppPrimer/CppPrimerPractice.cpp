b70e75c0bd2121335a7e5a9fc88f11d38312811a
vivi1 <wwei6@vt.edu>
2021-03-14 21:45:31 -0400

    E8.6

--- CppPrimer/CppPrimerPractice.cpp
+++ CppPrimer/CppPrimerPractice.cpp
@@ -3364,7 +3364,7 @@ for (auto i : test) cout << i << "\n";
 //E8.6: Rewrite the bookstore program from § 7.1.1 (p. 256) to read its
 //transactions from a file. Pass the name of the file as an argument to
 //main (§ 6.2.5, p. 218).
-
+//Please see my E8_6.cpp
 
 
 return 0; //return EXIT_SUCCESS
---
+++ CppPrimer/E8_6.cpp
@@ -0,0 +1,8 @@
+//E8.6: Rewrite the bookstore program from § 7.1.1 (p. 256) to read its
+//transactions from a file. Pass the name of the file as an argument to
+//main (§ 6.2.5, p. 218).
+
+int main() {
+
+  return 0;
+}
