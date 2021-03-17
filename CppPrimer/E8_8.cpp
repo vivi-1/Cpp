2e940f4d6bb3ff9551ca9ebd78abbd367e2bbcd8
vivi1 <wwei6@vt.edu>
2021-03-17 18:26:14 -0400

    E8_8.cpp

--- CppPrimer/E8_8.cpp
+++ CppPrimer/E8_8.cpp
@@ -1,6 +1,6 @@
-//E8.7: Revise the bookstore program from the previous section to write its
-//output to a file. Pass the name of that file as a second argument to main.
-
+//E8.8: Revise the program from the previous exercise to append its output to
+//its given file. Run the program on the same output file at least twice to
+//ensure that the data are preserved.
 // g++ was used as a compiler on OS Mac BigSur, the excutable file is default to be
 //a.out, I'm using test.txt and output.txt as parameters that are passed to argv.
 
@@ -13,7 +13,7 @@ using std::ifstream;
 using std::ofstream;
 
 int main(int argc, char **argv) {
-  ofstream output(argv[1]);
+  ofstream output(argv[1], ofstream::app);
   ifstream input (argv[2]);
   if (argc == 1 || argc == 2) {
     cerr << "Wrong input, has to have three parameters, including the excutable file and input and output files\n";
