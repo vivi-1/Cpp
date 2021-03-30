288be24ee68306bbf551f623b2f663293234d4b7
vivi1 <wwei6@vt.edu>
2021-03-29 22:00:23 -0400

    E9.28

--- CppPrimer/Chapter9_SequentialContainers/Chapter 9.cpp
+++ CppPrimer/Chapter9_SequentialContainers/Chapter 9.cpp
@@ -75,7 +75,6 @@ void find_replace(forward_list<string> flist, string s1, string s2) {
   }
 }
 
-
 int main(){
 //E9.1 Which is the most appropriate—a vector, a deque, or a list—for the
 //following program tasks? Explain the rationale for your choice. If there is
