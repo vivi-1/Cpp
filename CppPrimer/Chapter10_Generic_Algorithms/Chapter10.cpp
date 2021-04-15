1b4587cdb4636954fca40d69f2b1c965881b49d1
vivi-1 <wwei6@vt.edu>
2021-04-14 20:23:41 -0400

    E10.2

--- CppPrimer/Chapter10_Generic_Algorithms/Chapter10.cpp
+++ CppPrimer/Chapter10_Generic_Algorithms/Chapter10.cpp
@@ -2,6 +2,7 @@
 #include<algorithm>
 #include<vector>
 #include<list>
+#include<sstream>
 using namespace std;
 
 int main(){
@@ -9,13 +10,22 @@ int main(){
 //takes a pair of iterators and a value. count returns a count of how often that
 //value appears. Read a sequence of ints into a vector and print the count of
 //how many elements have a given value.
+//E10.2 Repeat the previous program, but read values into a list of strings.
 int input;
 vector<int> vec;
-while(cin >> input) vec.push_back(input);
+list<string> list;
+while(cin >> input) {
+  stringstream ss;
+  ss << input;
+  string sinput;
+  ss >> sinput;
+  vec.push_back(input);
+  list.push_back(sinput);
+}
 int search_val = 0;
 cout << search_val << " is present for " <<  count(vec.cbegin(), vec.cend(),search_val) << " times\n";
+cout << search_val << " is present for " << count(list.begin(), list.end(), to_string(search_val)) << " times\n";
 
-//E10.2 Repeat the previous program, but read values into a list of strings.
 
 
 return 0;
