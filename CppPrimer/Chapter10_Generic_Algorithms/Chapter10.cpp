9fe5c67adcd3805553d69760ca5099e1c2be155f
vivi-1 <wwei6@vt.edu>
2021-04-25 21:57:57 -0400

    E10.33

--- CppPrimer/Chapter10_Generic_Algorithms/Chapter10.cpp
+++ CppPrimer/Chapter10_Generic_Algorithms/Chapter10.cpp
@@ -388,13 +388,13 @@ while(i != vec13.end()) {
 */
 
 
-
 //E10.33 Write a program that takes the names of an input file and two output
 //files. The input file should hold integers. Using an istream_iterator read the
 // input file. Using ostream_iterators, write the odd numbers into the first
 //output file. Each value should be followed by a space. Write the even numbers
 //into the second file. Each of these values should be placed on a separate line.
 //Please see the file E10_33.cpp
+
 
 return 0;
 }
