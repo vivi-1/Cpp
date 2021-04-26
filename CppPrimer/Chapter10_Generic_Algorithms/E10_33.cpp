af86df1c50a8171212f303db681e0e77f550acb2
vivi-1 <wwei6@vt.edu>
2021-04-25 21:35:38 -0400

    //E10.33

--- CppPrimer/Chapter10_Generic_Algorithms/E10_33.cpp
+++ CppPrimer/Chapter10_Generic_Algorithms/E10_33.cpp
@@ -3,6 +3,29 @@
 // input file. Using ostream_iterators, write the odd numbers into the first
 //output file. Each value should be followed by a space. Write the even numbers
 //into the second file. Each of these values should be placed on a separate line.
-int main(){
-  
+#include <iostream>
+#include <fstream>
+#include <cstring>
+using namespace std;
+using std::ifstream;
+using std::ofstream;
+int main(int argc, char* argv[]){
+  ifstream input (argv[1]);
+  ofstream output_odd(argv[2], ofstream::app);
+  ofstream output_even(argv[3], ofstream::app);
+  if(argc!=4) {
+    cerr << "Wrong input, has to have four parameters, including the excutable file and input and output files\n";
+    return -1;
+  }
+  if(!input) {
+    cerr << "can't read file " << string(argv[1]) << endl;
+    return -2;
+  }
+
+  else{
+    istream_iterator<int> iter(input), eof;
+    while(iter!=eof) (*iter%2) ? print(output_odd, *iter++) : print(output_even, *iter++);
+  }
+
+return 0;
 }
