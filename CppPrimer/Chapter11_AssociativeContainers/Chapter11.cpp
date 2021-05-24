b1faa8ae8ad026f8921cb21b2d9270adf1add056
vivi-1 <wwei6@vt.edu>
2021-05-24 16:13:57 -0400

    E11.33

--- CppPrimer/Chapter11_AssociativeContainers/Chapter11.cpp
+++ CppPrimer/Chapter11_AssociativeContainers/Chapter11.cpp
@@ -11,6 +11,24 @@
 using namespace std;
 
 //E11.33
+map<string, string> buildMap(ifstream&map_file){
+  map<string, string> trans_map;
+  string key, value;
+  while(map_file >> key && getline(map_file, value))
+    if(value.size() > 1)
+      trans_map[key] = value.substr(1);
+    else
+      throw runtime_error("no rule for" + key);
+  return trans_map;
+}
+
+const string& transform(const string&s, const map<string, string>&m){
+  auto map_it = m.find(s);
+  if(map_it!=m.cend())
+    return map_it->second;
+  else
+    return s;
+}
 
 void word_transform(ifstream &map_file, ifstream& input){
   auto trans_map = buildMap(map_file);
