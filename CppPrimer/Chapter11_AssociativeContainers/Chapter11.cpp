067561c65585c206deb364929444ce0e34f15d08
vivi-1 <wwei6@vt.edu>
2021-05-26 21:02:07 -0400

    E11.38

--- CppPrimer/Chapter11_AssociativeContainers/Chapter11.cpp
+++ CppPrimer/Chapter11_AssociativeContainers/Chapter11.cpp
@@ -1,5 +1,6 @@
 #include<iostream>
 #include<map>
+#include <unordered_map>
 #include<set>
 #include<string>
 #include<vector>
@@ -42,6 +43,50 @@ void word_transform(ifstream &map_file, ifstream& input){
       if(firstword) firstword = false;
       else cout<<" ";
       cout << transform(word, trans_map);
+    }
+    cout << endl;
+  }
+}
+
+//E11.38 Rewrite the word-counting (§ 11.1, p. 421) and word-transformation
+//(§ 11.3.6, p. 440) programs to use an unordered_map.
+unordered_map<string, size_t> word_count6(){
+  string word;
+  unordered_map<string, size_t> word_count;
+  while (cin >> word) ++word_count[word];
+  for (const auto &w : word_count)
+    cout <<  w.first << " occurs " << w.second
+         << ((w.second > 1) ? " times" : " time") << endl;
+  return word_count;
+}
+
+const string& transformC(const string&s, const unordered_map<string, string>&m){
+  auto map_it = m.find(s);
+  if(map_it!=m.cend())
+    return map_it->second;
+  else
+    return s;
+}
+
+void word_transformC(ifstream &map_file, ifstream &input){
+  unordered_map<string, string> trans_map;
+  string key, value;
+  while(map_file >> key && getline(map_file, value))
+    if(value.size() > 1)
+      trans_map[key] = value.substr(1);
+    else
+      throw runtime_error("no rule for" + key);
+
+  string text;
+  while (getline(input, text)) {
+    istringstream stream(text);
+    string word;
+    bool firstword = true;
+    while (stream >> word) {
+    if (firstword) firstword = false;
+    else
+      cout << " ";
+      cout << transformC(word, trans_map);
     }
     cout << endl;
   }
@@ -378,33 +423,7 @@ for(auto i : order_map){
 // ordered one takes up O(logN).
 //Hash functions make it faster if the key is strings.
 
-//E11.38 Rewrite the word-counting (§ 11.1, p. 421) and word-transformation
-//(§ 11.3.6, p. 440) programs to use an unordered_map.
-void word_count6(){
-  string word;
-  unordered_map <string, size_t> word_count;
-  while (cin >> word) ++word_count[word];
-  for (const auto &w : word_count)
-    cout <<  w.first << " occurs " << w.second
-         << ((w.second > 1) ? " times" : " time") << endl;
-}
 
-void word_transform(ifstream &map_file, ifstream &input){
-  auto trans_map = buildMap(map_file);
-  string text;
-  while (getline(input, text)) {
-    istringstream stream(text);
-    string word;
-    bool firstword = true;
-    while (stream >> word) {
-    if (firstword) firstword = false;
-    else
-      cout << " ";
-      cout << transform(word, trans_map);
-    }
-    cout << endl;
-  }
-}
 
 
 return 0;
