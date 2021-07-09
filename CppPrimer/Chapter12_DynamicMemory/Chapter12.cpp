0aec014fa59f27ca56e1f1a634a0648b53e0edb0
vivi-1 <wwei6@vt.edu>
2021-07-08 22:18:20 -0400

    Questions

--- CppPrimer/Chapter12_DynamicMemory/Chapter12.cpp
+++ CppPrimer/Chapter12_DynamicMemory/Chapter12.cpp
@@ -7,11 +7,37 @@
 #include<utility>
 #include <chrono>
 #include <typeinfo>
-#include<fstream>
+#include <fstream>
 #include"Sales_item.h"
 #include <sstream>
+#include <memory>
 using namespace std;
 int main(){
+
+//E12.1 How many elements do b1 and b2 have at the end of this code?
+  // StrBlob b1;
+  // {
+  //     StrBlob b2 = {"a", "an", "the"};
+  //     b1 = b2;
+  //     b2.push_back("about");
+  // }
+
+
+
+//E12.2 Write your own version of the StrBlob class including the const versions
+//of front and back.
+
+//E12.3 Does this class need const versions of push_back and pop_back? If so,
+//add them. If not, why aren’t they needed?
+
+//E12.4 In our check function we didn’t check whether i was greater than zero.
+//Why is it okay to omit that check?
+
+//E12.5 We did not make the constructor that takes an initializer_list explicit
+//(§ 7.5.4, p. 296). Discuss the pros and cons of this design choice.
+
+
+
 
 
   return 0;
